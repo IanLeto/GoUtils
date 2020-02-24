bdecdacc2bef7ebf5719f9f02f6b83e8bcc56021
lan <lan@canway.net>
2020-02-24 20:27:26 +0800

    add goroutine leak

---
+++ currencyInGo/goroutineDemo.go
@@ -0,0 +1,38 @@
+package currencyInGo
+
+import (
+	"fmt"
+	"time"
+)
+
+func DoWork(strChan <-chan string) <-chan interface{} {
+	completed := make(chan interface{})
+	go func() {
+		defer fmt.Println("work done")
+		defer close(completed)
+		for value := range strChan {
+			fmt.Println(value)
+		}
+	}()
+	return completed
+}
+
+// 这里strchan 是否只读是无所谓的
+func DoWorkWithNoLeak(strChan chan string, done chan interface{}) chan interface{} {
+	completed := make(chan interface{})
+	go func() {
+		defer fmt.Println("work done")
+		defer close(completed)
+		for {
+			select {
+			case s := <-strChan:
+				fmt.Println(s)
+			case <-time.After(1 * time.Second):
+				fmt.Println("time out") //超时
+
+			case <-done:
+				return // 关闭goroutine 的信号
+			}
+		}
+	}()
+}
---
+++ currencyInGo/goroutineDemo_test.go
@@ -0,0 +1,29 @@
+package currencyInGo
+
+import (
+	"github.com/stretchr/testify/suite"
+	"testing"
+	"time"
+)
+
+type TestgoroutineDemo_testSuite struct {
+	suite.Suite
+}
+
+func (s *TestgoroutineDemo_testSuite) TestFunc() {
+	str := make(chan string, 0)
+	DoWork(str)
+	str <- "x"
+	time.Sleep(4 * time.Second)
+	close(str) // 如果不加上这一句，dowork 中的goroutine 将回泄漏
+	time.Sleep(4 * time.Second)
+
+}
+
+func (s *TestgoroutineDemo_testSuite) SetupTest() {
+
+}
+
+func TestGoroutineDemoSuite(t *testing.T) {
+	suite.Run(t, new(TestgoroutineDemo_testSuite))
+}
